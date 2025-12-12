85576b01e517f669f29ebfee90412e225dcb76ce
Aryan <rnshahi34@gmail.com>
2025-12-12 10:31:44 +0545

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+print("""this is  a 
+multi line 
+string""")
+
+def print_statement(str:str, mode:int):
+    if int:
+        print(f"""{str}""")
+    else:
+        print(f"{str}")
+
+
+print_statement("Hello World",0)
+print_statement("Hello " \
+"World", 1)
---
+++ readme.md
@@ -0,0 +1,15 @@
+#Python Notes
+
+
+
+**Primitive Data Types**
+int -> 3
+float ->3.56
+str -> ""
+bool -> True False
+
+**Dyamic Typing**
+```python
+name= "Aryan"
+name = 20
+```
